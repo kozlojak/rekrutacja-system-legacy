6dad5ae87184067936486f406e7819c70079a300
kozlojak <jakub3008@gmail.com>
2018-07-02 08:19:25 +0200

    Add OMGParser

---
+++ src/main/java/parser/OMGParser.java
@@ -0,0 +1,14 @@
+package parser;
+
+import to.OMG_TO;
+
+/**
+ *@author kozlojak
+ */
+
+public class OMGParser implements Parser<OMG_TO> {
+    @Override
+    public void parseMessage(OMG_TO message) {
+        //perform actions on realMessage
+    }
+}
