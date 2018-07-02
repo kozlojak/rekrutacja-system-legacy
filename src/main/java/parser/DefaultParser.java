3b0045773863379c36d1c3f0735a0a8e4e5274a2
kozlojak <jakub3008@gmail.com>
2018-07-02 08:19:41 +0200

    Add DefaultParser

---
+++ src/main/java/parser/DefaultParser.java
@@ -0,0 +1,16 @@
+package parser;
+
+import to.Default_TO;
+
+/**
+ * Example ofservice to parse the AMGEntity instance.
+ *
+ *@author kozlojak
+ */
+
+public class DefaultParser implements Parser<Default_TO>{
+    @Override
+    public void parseMessage(Default_TO message) {
+        //perform actions on realMessage
+    }
+}
