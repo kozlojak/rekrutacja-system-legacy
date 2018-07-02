fed01dbbc498a6675226158793b871d06bee9428
kozlojak <jakub3008@gmail.com>
2018-07-02 08:19:09 +0200

    Add ASTParser

---
+++ src/main/java/parser/ASTParser.java
@@ -0,0 +1,14 @@
+package parser;
+
+import to.AST_TO;
+
+/**
+ *@author kozlojak
+ */
+
+public class ASTParser implements Parser<AST_TO> {
+    @Override
+    public void parseMessage(AST_TO message) {
+        //perform actions on realMessage
+    }
+}
