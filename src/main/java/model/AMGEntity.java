cf819935d488f95296f48ecc81f16d9a8008702b
kozlojak <jakub3008@gmail.com>
2018-07-02 08:50:25 +0200

    Add AMG Entity

---
+++ src/main/java/model/AMGEntity.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.Data;
+
+/**
+ * Example of Entity to show the example of how the application could be designed.
+ *
+ * @author kozlojak
+ */
+
+@Data
+public class AMGEntity {
+}
