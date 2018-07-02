3088d8a195277a6f6c6b6d6d59a6d257f7e85736
kozlojak <jakub3008@gmail.com>
2018-07-02 08:18:53 +0200

    Add ASLParser

---
+++ src/main/java/parser/ASLParser.java
@@ -0,0 +1,14 @@
+package parser;
+
+import to.ASL_TO;
+
+/**
+ *@author kozlojak
+ */
+
+public class ASLParser implements Parser<ASL_TO>{
+    @Override
+    public void parseMessage(ASL_TO message) {
+        //perform actions on realMessage
+    }
+}
